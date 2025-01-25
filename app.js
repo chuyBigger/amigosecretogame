4f3612ecc877c5f2874075e045eb0a94ec3eee40
Jesus Medina Casas <chuytec15@gmail.com>
2025-01-25 00:36:26 -0600

    funcionesbasicas'

--- app.js
+++ app.js
@@ -1,1 +1,106 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Agregar nombres: 
+// Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar".
+
+// Validar entrada: 
+// Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
+
+// Visualizar la lista:
+//  Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
+
+// Sorteo aleatorio: 
+// Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página.
+let input = document.querySelector('#amigo').value;
+let amigos = [];
+
+
+function agregarAmigo(){
+  let input = document.querySelector('#amigo').value;
+  if(input === ''){
+    alert("Agrega un Nombre");
+   return false;
+   }else{
+    amigos.push(input);
+    cleanBox();
+    friendList();
+   }
+  
+}
+
+function cleanBox(){
+  let box = document.getElementById('amigo');
+  box.value = '';
+}
+
+function cleanList(){
+  let list = document.getElementById('listaAmigos');
+  list.value = '';
+}
+
+function friendList(){
+  const list = document.getElementById('listaAmigos');
+  cleanList();
+  for (let i = 0; i < amigos.length; i++) {
+    list.innerHTML += `<li>${amigos[i]}</li>`;
+  }
+  console.log(list)
+}
+
+//   let namesAmigos
+//   if ( namesAmigos == '' ){
+//       alert('Ingresa tienes que ingresar el nombre de un amigo')
+//     } else {
+//         amigos.push(document.getElementById('amigo').value);
+        
+//         amigos.innerHTML += `{'listaAmigos.value'}`;
+
+
+//         console.log(amigos)
+//     }
+// }
+
+
+// let listaDeAmigos = document.querySelector('#listaAmigos');
+// listaDeAmigos.innerHTML == 'listaAmigos'
+
+
+// function agregarNombre(){
+//     amigos = document.getElementById('amigo').value;
+
+// }
+
+
+// function recorrerListaAmigos() {
+//     let ul = document.getElementById('listaAmigos');
+  
+//     ul.innerHTML = '';
+  
+//     amigos.forEach(amigo => {
+//       let li = document.createElement('li');
+      
+//       li.textContent = amigo;
+      
+//       li.classList.add('amigo-item');
+  
+//       ul.append(li);
+  
+//     });
+//   }
+
+
+
+
+
+// function limpiarInput() {
+//     document.getElementById('amigo').value = '';
+//   }
+
+// function limpiarListaAmigos() {
+//     let listaAmigos = document.getElementById('listaAmigos');
+  
+//     while (listaAmigos.firstChild) {
+//       listaAmigos.removeChild(listaAmigos.firstChild);
+//     }
+// }
